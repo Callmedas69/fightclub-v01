c7e905e19d3fcff3a2908a834adc58c9b1f760f7
0xDas <126429597+Callmedas69@users.noreply.github.com>
2024-10-07 22:11:20 +0700

    onchaitkitprovider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { OnchainKitProvider } from "@coinbase/onchainkit";
+
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { ReactNode } from "react";
-import { base } from "viem/chains";
 import { WagmiProvider } from "wagmi";
-import { NEXT_PUBLIC_CDP_API_KEY } from "../../config";
 import { useWamigConfig } from "../../wagmi";
 import { ThirdwebProvider } from "thirdweb/react";
 
@@ -19,11 +17,9 @@ function OnchainProviders({ children }: Props) {
   return (
     <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
-        <OnchainKitProvider apiKey={NEXT_PUBLIC_CDP_API_KEY} chain={base}>
-          <RainbowKitProvider>
-            <ThirdwebProvider>{children}</ThirdwebProvider>
-          </RainbowKitProvider>
-        </OnchainKitProvider>
+        <RainbowKitProvider>
+          <ThirdwebProvider>{children}</ThirdwebProvider>
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
